3e6a2a53c577c2af3fae49127eaccc29d749cec5
Dtake88 <56423564+Dtake88@users.noreply.github.com>
2021-04-16 12:55:11 +0700

    halaman awal
    tinggal tunggu database

---
+++ resources/views/layout/headerlayout1.blade.php
@@ -0,0 +1,37 @@
+<nav class="navbar default-layout col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+    <div class="text-center navbar-brand-wrapper d-flex align-items-top justify-content-center">
+      <a class="navbar-brand brand-logo" href="{{ url('/') }}">
+        <img src="{{ url('assets/images/logo.svg') }}" alt="logo" /> </a>
+      <a class="navbar-brand brand-logo-mini" href="{{ url('/') }}">
+        <img src="{{ url('assets/images/logo-mini.svg') }}" alt="logo" /> </a>
+    </div>
+    <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+      <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+        <span class="mdi mdi-menu"></span>
+      </button>
+      <ul class="navbar-nav navbar-nav-left header-links">
+        <li class="nav-item d-none d-xl-flex">
+
+          </a>
+        </li>
+        <li class="nav-item active d-none d-lg-flex">
+
+        </li>
+        <li class="nav-item d-none d-md-flex">
+
+        </li>
+        <li class="nav-item dropdown d-none d-lg-flex">
+
+        </li>
+      </ul>
+      <ul class="navbar-nav navbar-nav-right">
+        <li class="nav-item dropdown d-none d-xl-inline-block">
+            <a class="dropdown-item text-white"><button type="button" class="btn btn-light btn-fw">Login</button> </a>
+
+        </li>
+      </ul>
+      <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+        <span class="mdi mdi-menu icon-menu"></span>
+      </button>
+    </div>
+  </nav>
